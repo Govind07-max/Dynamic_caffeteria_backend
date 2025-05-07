61132d8461d2ffda047ce9bdaad18fe287d0f091
govind suryawanshi <govindsr051@gmail.com>
2025-05-08 01:49:18 +0530

    booking id

--- controllers/seatbookController.js
+++ controllers/seatbookController.js
@@ -3,10 +3,10 @@ import { SeatBooking } from "../config/seatBookModel.js";
 import { User } from "../config/userModel.js";
 
 const bookTable = asyncHandler(async (req, res) => {
-    const { tableType, date, time, name, contact, advancePayment } = req.body;
+    const { tableType, date, time, name, contact, advancePayment, payment_id } = req.body;
 
     // Validate required fields
-    if (!tableType || !date || !time || !name || !contact || !advancePayment) {
+    if (!tableType || !date || !time || !name || !contact || !advancePayment || !payment_id) {
         res.status(400);
         throw new Error("Please fill all the required fields");
     }
@@ -26,11 +26,14 @@ const bookTable = asyncHandler(async (req, res) => {
         name,
         contact,
         advancePayment,
+        payment_id,
         user: req.user._id
     });
 
     // Emit the booking event to all connected clients
     req.io.emit("newBooking", {
+        
+        payment_id: booking.payment_id,
         tableType: booking.tableType,
         date: booking.date,
         time: booking.time,
